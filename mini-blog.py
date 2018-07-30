f4995b0623c46fe4bd20005a53b740b677afa7c4
Gordonbalaaka <balaakagordon@gmail.com>
2018-07-30 12:26:57 +0300

    Update edit entry method

--- mini-blog.py
+++ mini-blog.py
@@ -1,14 +1,14 @@
 import datetime
 
 user = [{"usrnm":"user1", "passwd":"pass1", "us_id":1, "us_type":"admin"}]
-E = [{e_id:1, message:"bla", time:'timestamp', author:1, parent:None}]
+E = [{"e_id":1, "message":"bla", "time":'timestamp', "author":1, "parent":None}]
 
 def edit_comment(user, comment, new_data):
 	if current_user["us_type"] == "admin":
 		comment["message"] = new_data
 		date = datetime.datetime.now()
 		timestamp = date.strftime("%Y-%m-%d %H:%M")
-		comment["timestamp"] = timestamp
+		comment["time"] = timestamp
 		message = "Comment updated"
 	else:
 		message = "You are not authorized to edit this comment"
